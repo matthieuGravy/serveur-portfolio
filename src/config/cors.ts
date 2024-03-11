47647e7c8f8b496b8c79f212330ebc9eeb1742a4
matthieuGravy <matthieugravy@gmail.com>
2024-03-11 12:52:45 +0100

    preparation du env

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -2,6 +2,7 @@ import express from "express";
 const cors = require("cors");
 
 const corsOptions = {
+  //  origin: process.env.NODE_ENV === 'production' ? ['http://your-production-domain.com'] : '*',
   origin: ["http://localhost:5173"],
   methods: "POST",
   credentials: true,
